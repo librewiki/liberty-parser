f8397d09162ed40a9b222bfce6dd9d119de33c1d
damezuma <qweads12@gmail.com>
2015-12-22 01:48:31 +0900

    지금까지 한 부분 추가

--- liberty-parser.js
+++ liberty-parser.js
@@ -12,17 +12,35 @@ var SPECIAL_LANGUAGE_INFO_REGEX = /\{\{([^\}]*)\}\}/g;
 var REFERENCE_REGEX = /<ref\s*name="([^"]*)"\s*\/?>[^<]*(<\/ref>)?/g;
 var REFERENCES_TO_IGNORE = /<ref[^\/]*\/>|<ref[^>]*>[^<]*<\/ref>/g;
 var COMMENTS_REGEX = /<!--\s*[^-]*-->/g;
-
+var NOWIKI_TAG_REGEX = /<nowiki>(+)</nowiki>/g;
 var LANGUAGE_MAP = {
 	"lang-fr": "French",
 	"lang-es": "Spanish",
 	"lang-en": "English",
 	"lang-it": "Italian"
 };
-
+function convertTableToHTML(text){
+	var tablePositions = [];
+	var idx = -1;
+	while((idx = text.indexOf("{|",idx)) != -1)
+	{
+		tablePositions.push({type:0,pos:idx});
+	}
+	while((idx = text.indexOf("|}",idx)) != -1)
+	{
+		var i = 0;
+		for(i = 0 ; i < tablePositions.length;i++)
+		{
+			
+		}
+	}
+}
 function convertLineToHTML(line, lang) {
 	var matched = false;
-
+	line = line.replace(NOWIKI_TAG_REGEX, function(match, matchedSequence){
+		matched = true;
+		return line.replace(/</gi,"&lt;").replace(/>/gi,"&gt;");
+	};
 	line = line.replace(CITATION_REFERENCE_REGEX, function(match, matchedSequence) {
 		var sections = matchedSequence.split("|");
 		var refType = sections.shift();
@@ -190,3 +208,142 @@ function convertLineToHTML(line, lang) {
 
 
 module.exports.convertLineToHTML = convertLineToHTML;
+
+
+
+//EDITING BY DAMEZUMA
+
+function NowikiNode(text)
+{
+    this.type = "NOWIKI";
+    this.text = text;
+}
+NowikiNode.prototype.toHtml = function(){
+  return this.text.replace(/</gi,"&lt;").replace(/>/gi,"&gt;");  
+};
+function TextNode(text)
+{
+    this.type = "TEXT";
+    this.text = text;
+}
+function HeadingNode(text)
+{
+    
+}
+function ArticleNode(text)
+{
+    
+}
+function LinkNode(text)
+{
+    
+}
+function ReferenceNode(text)
+{
+    
+}
+function TableNode(text)
+{
+    
+}
+function TemplateNode(text)
+{
+    
+}
+function NowikiParse(ROOT)
+{
+	var preTextExistNowikiStart = false;
+	if(ROOT.children != null)
+	{
+		for( i in ROOT.children)
+		{
+			var iter = ROOT.children[i];
+			if(iter.type == "TEXT")
+			{
+				var text = iter.text;
+				var t = text;
+				var nowikiList = [];
+				var idx = 0;
+				var nowikiRes = /<nowiki>/g;
+				var res = [];
+				var lastIdx = 0;
+				while((idx = text.search(nowikiRes)) != -1)
+				{
+					
+					nowikiList.push([0,lastIdx + idx]);
+					idx += 8;
+					text = text.slice(idx);
+					lastIdx += idx;
+				}
+				
+				nowikiRes = /<\/nowiki>/g;
+				text = t;
+				lastIdx = 0;
+				while((idx = text.search(nowikiRes)) != -1)
+				{
+					var isInsertCorrect = false;
+					for(i in nowikiList)
+					{
+						if(nowikiList[i][1] < idx)
+						{
+							nowikiList.insert(i, [1,lastIdx + idx]);
+							isInsertCorrect = true;
+							break;
+						}
+					}
+					if(isInsertCorrect == false)
+					{
+						nowikiList.push([1,lastIdx + idx]);
+					}
+					text = text.slice(idx + 9);
+					lastIdx += idx;
+				}
+				var openediter = null;
+				lastIdx = 0;
+				for(i in nowikiList)
+				{
+					var iter = nowikiList[i];
+					if(openediter == null || iter[0] == 0)
+					{
+						if(lastIdx != iter[1])
+						{
+							res.push(new TextNode(text.substr(lastIdx,iter[1])));
+						}
+						lastIdx = iter[1];
+						openediter = iter;
+					}
+					else if(openediter != null || iter[0] == 1)
+					{
+						var nowiki_node = new NowikiNode(text.substr(openediter[1] + 8,iter[1]));
+						res.push(nowiki_node);
+						openediter = null;
+						lastIdx = iter[1] + 10;
+					}
+					else
+					{
+						tag = "&lt;nowiki&gt;";
+						lastIdx = iter[1] + 8;
+						if(iter[0] == 1)
+						{
+							lastIdx = iter[1] + 9;
+							tag = "&lt;/nowiki&gt;";
+						}
+						res.push(new TextNode(tag));
+					}
+					
+				}
+				res.push(new TextNode(text.substr(lastIdx,text.length)));
+			}
+		}
+	}
+}
+function TemplateNodeParse(nodeList)
+{
+	
+}
+function Parse(text)
+{
+	var ROOT = {type:"ROOT",children:[new TextNode(text)]};
+	NowikiParse(ROOT);
+	TemplateNodeParse(ROOT);
+}
