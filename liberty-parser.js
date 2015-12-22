d1f349e297760fd2cf640c2073c02e109b0c30cb
damezuma <qweads12@gmail.com>
2015-12-22 22:54:49 +0900

    파싱 부분 추가

--- liberty-parser.js
+++ liberty-parser.js
@@ -1,4 +1,4 @@
-
+"USE STRICT"
 //EDITING BY DAMEZUMA
 
 function NowikiNode(text)
@@ -14,171 +14,35 @@ function TextNode(text)
     this.type = "TEXT";
     this.text = text;
 }
-function HeadingNode(text)
+function HeadingNode()
 {
     
 }
-function ArticleNode(text)
+function ArticleNode()
 {
     
 }
-function LinkNode(text)
+function LinkNode()
 {
     
 }
-function ReferenceNode(text)
+function ReferenceNode()
 {
     
 }
-function Insert(obj, index, item)
+function TableNode()
 {
-    var a = obj.splice(index);
-    obj.push(item);
-    for(i in a)obj.push(a[i]);
+	this.NAME = "TABLE";
+    this.children = [];
 }
-function TableNode(text)
+function TemplateNode()
 {
-    
+	this.NAME = "TEMPLATE";
+    this.children = [];
 }
-function TemplateNode(text)
+function LibertyMark()
 {
-    
-}
-function NowikiMark(text, markList){
-    var t = text;
-    var idx = 0;
-    var nowikiRes = /<nowiki>/g;
-    var res = [];
-    var lastIdx = 0;
-    while((idx = text.search(nowikiRes)) != -1){
-        markList.push({
-            type:"NOWIKI",
-            tag:"OPEN",
-            pos:lastIdx + idx
-        });
-        idx += 8;
-        text = text.slice(idx);
-        lastIdx += idx;
-    }
-    
-    nowikiRes = /<\/nowiki>/g;
-    text = t;
-    lastIdx = 0;
-    while((idx = text.search(nowikiRes)) != -1){
-        var isInsertCorrect = false;
-        var item = {
-            type:"NOWIKI",
-            tag:"CLOSE",
-            pos:lastIdx + idx
-        }
-        for(i in markList){
-            if(markList[i].pos > item.pos){
-               
-                Insert(markList,i,item);
-                isInsertCorrect = true;
-                break;
-            }
-        }
-        if(isInsertCorrect == false)markList.push(item);
-        
-        text = text.slice(idx + 9);
-        lastIdx += idx;
-    }
-}
-function TemplateMark(text,markList){
-    var idx;
-    var lastIdx = 0;
-    var t = text;
-	while((idx = text.indexOf("{{")) != -1){
-        var isInsertCorrect = false;
-        var item = {
-            type:"TEMPLATE",
-            tag:"OPEN",
-            pos:lastIdx + idx
-        }
-        for(i in markList){
-            if(markList[i].pos > item.pos){
-                Insert(markList,i,item);
-                isInsertCorrect = true;
-                break;
-            }
-        }
-        if(isInsertCorrect == false)markList.push(item);
-        
-        text = text.slice(idx + 2);
-        lastIdx += idx;
-    }
-    
-    text = t;
-    lastIdx = 0;
-    
-    while((idx = text.indexOf("}}")) != -1){
-        var isInsertCorrect = false;
-        var item = {
-            type:"TEMPLATE",
-            tag:"CLOSE",
-            pos:lastIdx + idx
-        }
-        for(i in markList){
-            if(markList[i].pos > item.pos){
-                Insert(markList,i,item);
-                isInsertCorrect = true;
-                break;
-            }
-        }
-        if(isInsertCorrect == false)markList.push(item);
-        text = text.slice(idx + 2);
-        lastIdx += idx;
-    }
-}
-function TableMark(text,markList){
-    var idx;
-    var lastIdx = 0;
-    var t = text;
-	while((idx = text.indexOf("{|-")) != -1){
-        var isInsertCorrect = false;
-        var item = {
-            type:"TABLE",
-            tag:"OPEN",
-            pos:lastIdx + idx
-        }
-        for(i in markList){
-            if(markList[i].pos > item.pos){
-                Insert(markList,i,item);
-                isInsertCorrect = true;
-                break;
-            }
-        }
-        if(isInsertCorrect == false) markList.push(item);
-        text = text.slice(idx + 3);
-        lastIdx += idx;
-    }
-    
-    text = t;
-    lastIdx = 0;
-    
-    while((idx = text.indexOf("-|}")) != -1){
-        var isInsertCorrect = false;
-        var item = {
-            type:"TABLE",
-            tag:"CLOSE",
-            pos:lastIdx + idx
-        }
-        for(i in markList){
-            if(markList[i].pos > item.pos)
-            {
-                Insert(markList,i,item);
-                isInsertCorrect = true;
-                break;
-            }
-        }
-        if(isInsertCorrect == false)markList.push(item);
-        text = text.slice(idx + 3);
-        lastIdx += idx;
-    }
-}
-function MarkProcess(text,markList){
-    
+	this.children = [];
 }
 var MARK_TYPE = {
     STANDALONE:"STANDALONE",
@@ -186,29 +50,34 @@ var MARK_TYPE = {
     CLOSE_TAG:"CLOSE"
 };
 function HookMarker(hooker,markType){
-    this.hookker = hooker;
+	if(hooker == null) throw "hooker is null!";
+	if(markType == null) throw "mark type is null!";
+    this.hooker = hooker;
     this.markType = markType;
+	this.NAME = "HOOK MARKER";
 }
 function WikiParser(){
     this.hookers = [];
-    this.marklist = [];
+    this.markList = [];
 }
 WikiParser.prototype.AddHooker = function(hooker){
     this.hookers.push(hooker);
 };
-WikiParser.prototype.MarkPosition = function(marker,position){
+WikiParser.prototype.AddMark = function(marker,position){
     var isDoneInsert = false;
-    var item = {marker:marker,pos:position};
+    var item = {marker:marker,position:position};
     for(i in this.markList){
         var iter = this.markList[i];
-        if(iter.pos > position)
+        if(iter.position > position)
         {
-            Insert(this.markList,i,item);
+            var a = this.markList.splice(i);
+			this.markList.push(item);
+			for(i in a)this.markList.push(a[i]);
             isDoneInsert = true;
             break;
         }
     }
-    if(isDoneInsert){
+    if(isDoneInsert == false){
         this.markList.push(item);
     }
 };
@@ -216,26 +85,85 @@ WikiParser.prototype.DoBasicMarkTag = function(tagName){
     
 };
 WikiParser.prototype.Parse = function(text){
-    
+    for(i in this.hookers){
+		var hooker = this.hookers[i];
+		hooker.DoMark(this,text);
+	}
+	var stack = [];
+	var lastIdx = 0;
+	stack.push(new LibertyMark());
+	for(i in this.markList){
+		var iter = this.markList[i];
+		switch(iter.marker.markType)
+		{
+			case MARK_TYPE.CLOSE_TAG:{
+				if(stack.length == 1){
+					throw "parsing error! 문법이 틀렸다!";
+				}
+				var lastNode = stack[stack.length - 1];
+				stack.pop();
+				lastNode.children.push(new TextNode(text.substring(lastIdx, iter.position)));
+			}
+			break;
+			case MARK_TYPE.OPEN_TAG:
+				stack[stack.length - 1].children.push(new TextNode(text.substring(lastIdx, iter.position)));
+				var node = new iter.marker.hooker.NODE();
+				stack[stack.length - 1].children.push(node);
+				stack.push(node);
+			break;
+			case MARK_TYPE.STANDALONE:
+				stack[stack.length - 1].children.push(new iter.marker.hooker.NODE());
+			break;
+		}
+		lastIdx = iter.position;
+	}
+	return stack[0];
 };
 function NowikiHooker(){
-    
+    this.NAME = "NOWIKI HOOKER";
+	this.NODE = NowikiNode;
 };
 NowikiHooker.prototype.DoMark = function(wikiparser,text){
     wikiparser.DoBasicMarkTag("nowiki");
 };
-
+function TemplateHooker(){
+	this.NAME = "TAMPLATE HOOKER";
+	this.NODE = TemplateNode;
+}
+TemplateHooker.prototype.DoMark = function(wikiparser, text){
+	var idx = 0;
+	while((idx = text.indexOf("{{", idx)) != -1){
+        wikiparser.AddMark(new HookMarker(this, MARK_TYPE.OPEN_TAG),idx);
+		idx += 2;
+    }
+    idx = 0;
+    while((idx = text.indexOf("}}", idx)) != -1){
+		idx += 2;
+        wikiparser.AddMark(new HookMarker(this, MARK_TYPE.CLOSE_TAG),idx);
+    }
+};
+function TableHooker(){
+	this.NAME = "TABLE HOOKER";
+	this.NODE = TableNode;
+}
+TableHooker.prototype.DoMark = function(wikiparser, text){
+	var idx = 0;
+	while((idx = text.indexOf("{|", idx)) != -1){
+        wikiparser.AddMark(new HookMarker(this, MARK_TYPE.OPEN_TAG),idx);
+		idx += 2;
+    }
+    idx = 0;
+    while((idx = text.indexOf("|}", idx)) != -1){
+		idx += 2;
+        wikiparser.AddMark(new HookMarker(this, MARK_TYPE.CLOSE_TAG),idx);
+    }
+};
 function Parse(text){
     
     var wikiparser = new WikiParser();
     wikiparser.AddHooker(new NowikiHooker());
     wikiparser.AddHooker(new TemplateHooker());
     wikiparser.AddHooker(new TableHooker());
-    wikiparser.Parse(text);
-    
-    NowikiMark(text,markList);
-    TemplateMark(text,markList);
-    TableMark(text,markList);
-    var root = MarkProcess(text,markList);
-    console.log(markList);
+    console.log(wikiparser.Parse(text));
+    console.log(wikiparser.markList);
 }
