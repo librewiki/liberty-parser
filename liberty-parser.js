b1babc2adb4226a7bc4819299c4eb00600a31bc0
damezuma <qweads12@gmail.com>
2015-12-25 14:13:43 +0900

    insert기능을 찾았다

--- liberty-parser.js
+++ liberty-parser.js
@@ -5,128 +5,60 @@
 //Process()-> 노드를 HTML로 변환하기 이전에 해야 할 일들을 한다. 
 //테이블의 경우 각 셀을 분리하고, 템플릿의 경우에는 틀 이름과 매개변수를 정리한다
 
-function NowikiNode(text)
-{
+function NowikiNode(text){
     this.type = "NOWIKI";
     this.text = text;
 }
 NowikiNode.prototype.Render = function(){
   return this.text.replace(/</gi,"&lt;").replace(/>/gi,"&gt;");  
 };
-function TextNode(text)
-{
+function TextNode(text){
     this.type = "TEXT";
     this.text = text;
 }
 TextNode.prototype.Process = function(wikiparser){
-//텍스트 노드안에 있는 링크, 문단, 헤딩, 볼드체등을 파싱, 노드 트리구조로 만든 후 반환한다.
-//여기는 다시 짜야 한다.
-//텍스트를 char순회가 아니라 indexOf혹은 search를 이용하여 각 태그위치에 표시를 찍고 이것을 돌면서 파싱해야 한다.
-/*
-	var stack = [{
-        nodeClass:null,
-        pos:0,
-        children:[]
-    }];
-    var lastIdx = 0;
-    var i;
-	for(i = 0 ; i < this.text.length ; i++)
-	{
-		if(this.text.substr(i,3) == "'''")
-        {
-            if(stack[stack.length - 1].nodeClass != BoldNode)
-            {
-                var node = new TextNode(this.text.substring(lastIdx, i));
-                if(node.text.length != 0)
-                {
-                    stack[stack.length - 1].children.push(node);
-                }
-                stack.push({
-                    nodeClass:BoldNode,
-                    pos:i,
-                    children:[]
-                });
-                i+=2;
-                lastIdx = i + 1;
-            }
-            else
-            {
-                var item = stack.pop();
-                var node = new item.nodeClass();
-                var node2 = new TextNode(this.text.substring(lastIdx, i));
-                node.children = item.children;
-                node.children.push(node2);
-                node.Process(wikiparser);
-                stack[stack.length - 1].children.push(node);
-                lastIdx = i + 1;
-            }
-            
-        }
-	}
-    lastIdx++;
-    if(lastIdx < this.text.length - 1)
-    {
-        var node = new TextNode(this.text.substring(lastIdx, i));
-        if(node.text.length != 0)
-        {
-            stack[stack.length - 1].children.push(node);
-        }
-    }
-    return stack[0].children;
-	*/
+
 };
-TextNode.prototype.Render = function(wikiparser)
-{
+TextNode.prototype.Render = function(wikiparser){
 	return this.text;
 };
-function HeadingNode()
-{
+function HeadingNode(){
     
 }
-function ArticleNode()
-{
+function ArticleNode(){
     
 }
-function LinkNode()
-{
+function LinkNode(){
     
 }
 function RefNode()
 {
     
 }
-function ReferenceNode()
-{
+function ReferenceNode(){
 	
 };
 
-function BoldNode()
-{
+function BoldNode(){
 	this.type = "BOLD";
 	this.children = [];
 }
-BoldNode.prototype.Render = function(wikiparser)
-{
+BoldNode.prototype.Render = function(wikiparser){
 	var res = [];
-	if(this.children[0].type == "TEXT")
-	{
-		if(this.children[0].text.startsWith("'''"))
-		{
+	if(this.children[0].type == "TEXT"){
+		if(this.children[0].text.startsWith("'''")){
 			this.children[0].text = this.children[0].text.substr(3);
 		}
 	}
-	if(this.children[this.children.length - 1].type == "TEXT")
-	{
+	if(this.children[this.children.length - 1].type == "TEXT"){
 		var t = this.children[this.children.length - 1].text;
-		if(t.endsWith("'''"))
-		{
+		if(t.endsWith("'''")){
 			t = t.substring(0, t.length -3);
 			this.children[this.children.length - 1].text = t;
 		}
 	}
 	res.push("<b>");
-	for(i in this.children)
-	{
+	for(i in this.children){
 		
 		var it = this.children[i];
 		res.push(it.Render(wikiparser));
@@ -134,12 +66,10 @@ BoldNode.prototype.Render = function(wikiparser)
 	res.push("</b>");
 	return res.join("");
 };
-BoldNode.prototype.Process = function(wikiparser)
-{
+BoldNode.prototype.Process = function(wikiparser){
 	
 };
-function TableNode()
-{
+function TableNode(){
 	this.NAME = "TABLE";
     this.children = [];
 	this.cells = [];
@@ -153,50 +83,39 @@ TableNode.prototype.Process = function(){
 	var isStartCell = 0;
 	var posPreBar = -1;
 	var j = -1;
-	if(this.children[0] != null)
-	{
-		if(this.children[0].type == "TEXT")
-		{
+	if(this.children[0] != null){
+		if(this.children[0].type == "TEXT"){
 			var temp = this.children[0].text;
 			temp = temp.substring(2,temp.length);
 			var i = temp.indexOf("\n");
-			if(i != -1)
-			{
+			if(i != -1){
 				this.tableattr = temp.substring(0,i);
 				this.children[0].text = temp.substring(i,temp.length);
 			}
 		}
-		if(this.children[this.children.length - 1].type == "TEXT")
-		{
+		if(this.children[this.children.length - 1].type == "TEXT"){
 			var temp = this.children[this.children.length - 1].text;
 			this.children[this.children.length - 1].text = temp.substring(0,temp.length - 2);
 		}
 	}
 	var item = null;
-	for(i in this.children)
-	{
+	for(i in this.children){
 		var iter = this.children[i];
-		if(iter.type == "TEXT")
-		{
+		if(iter.type == "TEXT"){
 			posPreBar = -1;
 			var temp = iter.text;
 //테이블의 셀을 구한다.
 //셀 파싱 규칙은 '|속성|셀 내용\n'이 기본이며, '|셀 내용\n'이나 '||셀 내용'은 변칙일 뿐이다
-			for(j = 0 ; j < temp.length ; j++)
-			{
-				if(temp.substr(j,2) == "|-")
-				{
+			for(j = 0 ; j < temp.length ; j++){
+				if(temp.substr(j,2) == "|-"){
 					res.push([]);
 					row++;
-					
 					isStartCell = 0;
 					posPreBar = -1;
 					
 				}
-				else if(temp[j] == "|")
-				{
-					switch(isStartCell)
-					{
+				else if(temp[j] == "|"){
+					switch(isStartCell){
 						case 0:{
 							posPreBar = j;
 							isStartCell = 1;
@@ -222,54 +141,44 @@ TableNode.prototype.Process = function(){
 						break;
 					}
 				}
-				else if(temp[j] == '\n' )
-				{
-					if(isStartCell != 0)
-					{
-						var t = temp.substring(posPreBar+1,j);
-						var newTextNode = new TextNode(t);
-						item.children.push(newTextNode);
-						res[row].push(item);
-						children.push(newTextNode);
-						isStartCell = 0;
-						posPreBar = j;
-					}
+				else if(temp[j] == '\n' && isStartCell != 0){
+					var t = temp.substring(posPreBar+1,j);
+					var newTextNode = new TextNode(t);
+					item.children.push(newTextNode);
+					res[row].push(item);
+					children.push(newTextNode);
+					isStartCell = 0;
+					posPreBar = j;
 				}
 			}
-			if(isStartCell != 0)
-			{
+			if(isStartCell != 0){
 				var t = temp.substring(posPreBar+1,temp.length);
 				var newTextNode = new TextNode(t);
 				item.children.push(newTextNode);
 				children.push(newTextNode);
 			}
 		}
-		else
-		{
+		else{
 			item.children.push(iter);
 			children.push(iter);
 		}
 	}
-	for(i in res)
-	{
+	for(i in res){
 		if(res[i].length != 0){
 			this.cells.push(res[i]);
 		}
 	}
 	this.children = children;
 };
-TableNode.prototype.Render = function(wikiparser)
-{
+TableNode.prototype.Render = function(wikiparser){
 	var res = [];
 	res.push("<table ");
 	res.push(this.tableattr);
 	res.push(">");
-	for(i in this.cells)
-	{
+	for(i in this.cells){
 		var row = this.cells[i];
 		res.push("<tr>");
-		for(j in row)
-		{
+		for(j in row){
 			var cell = row[j];
 			res.push("<td ");
 			res.push(cell.attr);
@@ -286,26 +195,22 @@ TableNode.prototype.Render = function(wikiparser)
 	res.push("</table>");
 	return res.join("");
 };
-function TemplateNode(hooker)
-{
+function TemplateNode(hooker){
 	this.NAME = "TEMPLATE";
     this.children = [];
 	this.hooker = hooker;
 }
-TemplateNode.prototype.Process = function()
-{
+TemplateNode.prototype.Process = function(){
 	
 };
-TemplateNode.prototype.Render = function(wikiparser)
-{
+TemplateNode.prototype.Render = function(wikiparser){
 	return "[템블릿 있던 자리]";
 };
 function LibertyMark()
 {
 	this.children = [];
 }
-LibertyMark.prototype.Render = function(wikiparser)
-{
+LibertyMark.prototype.Render = function(wikiparser){
 	res = [];
 	for(i in this.children)
 	{
@@ -338,11 +243,8 @@ WikiParser.prototype.AddMark = function(marker,position){
     var item = {marker:marker,position:position};
     for(i in this.markList){
         var iter = this.markList[i];
-        if(iter.position > position)
-        {
-            var a = this.markList.splice(i);
-			this.markList.push(item);
-			for(i in a)this.markList.push(a[i]);
+        if(iter.position > position){
+            this.markList.splice(i,0,item);
             isDoneInsert = true;
             break;
         }
@@ -368,8 +270,7 @@ WikiParser.prototype.Parse = function(text){
 	stack.push(new LibertyMark());
 	for(i in this.markList){
 		var iter = this.markList[i];
-		switch(iter.marker.markType)
-		{
+		switch(iter.marker.markType){
 			case MARK_TYPE.CLOSE_TAG:{
 				if(stack.length == 1){
 					throw "parsing error! 문법이 틀렸다!";
@@ -393,9 +294,7 @@ WikiParser.prototype.Parse = function(text){
 		lastIdx = iter.position;
 	}
     if(lastIdx <= text.length -1)
-    {
-        stack[stack.length - 1].children.push(new TextNode(text.substring(lastIdx, text.length)));
-    }
+		stack[stack.length - 1].children.push(new TextNode(text.substring(lastIdx, text.length)));
 	
 	
 	return this.TextNodeParse(stack[0]);
@@ -411,15 +310,6 @@ function TemplateHooker(){
 	this.NAME = "TAMPLATE HOOKER";
 	this.NODE = TemplateNode;
 }
-TemplateHooker.prototype.GetStartStrLen = function(text)
-{
-	return 2;
-};
-TemplateHooker.prototype.GetEndStrLen = function(text)
-{
-	return 2;
-};
-
 TemplateHooker.prototype.DoMark = function(wikiparser, text){
 	var idx = 0;
 	while((idx = text.indexOf("{{", idx)) != -1){
@@ -466,12 +356,10 @@ BoldTagHooker.prototype.DoMark = function(wikiparser,text){
 	var isStartTag = false;
 	while((idx = text.indexOf("'''", idx)) != -1){
 		var tagType = MARK_TYPE.OPEN_TAG;
-		if(!isStartTag)
-		{
+		if(!isStartTag){
 			wikiparser.AddMark(new HookMarker(this, MARK_TYPE.OPEN_TAG),idx);
 		}
-		else
-		{
+		else{
 			wikiparser.AddMark(new HookMarker(this, MARK_TYPE.CLOSE_TAG),idx + 3);
 		}
 		isStartTag = !isStartTag;
@@ -483,12 +371,10 @@ BRTagHooker.prototype.DoMark = function(wikiparser,text){
 	var isStartTag = false;
 	while((idx = text.indexOf("'''", idx)) != -1){
 		var tagType = MARK_TYPE.OPEN_TAG;
-		if(!isStartTag)
-		{
+		if(!isStartTag){
 			wikiparser.AddMark(new HookMarker(this, MARK_TYPE.OPEN_TAG),idx);
 		}
-		else
-		{
+		else{
 			wikiparser.AddMark(new HookMarker(this, MARK_TYPE.CLOSE_TAG),idx + 3);
 		}
 		isStartTag = !isStartTag;
